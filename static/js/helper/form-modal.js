161a98e37db4887a4f1bc891528449ad94438df4
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-03-03 23:35:24 +0800

    Add helper function to open form modal

---
+++ static/js/helper/form-modal.js
@@ -0,0 +1,36 @@
+async function openFormModal({
+  title,
+  html,
+  onOpen = undefined,
+  onSubmit = undefined,
+  callback = undefined,
+  confirmButtonText = 'Ok',
+  cancelButtonText = 'Cancel',
+  showCancelButton = true,
+  showCloseButton = false,
+  allowOutsideClick = true,
+}) {
+  const result = await Swal.fire({
+    title,
+    html,
+    didOpen: () => {
+      if (onOpen !== undefined) {
+        onOpen()
+      }
+    },
+    preConfirm: () => {
+      if (onSubmit !== undefined) {
+        onSubmit()
+      }
+    },
+    confirmButtonText,
+    cancelButtonText,
+    showCancelButton,
+    showCloseButton,
+    allowOutsideClick,
+  })
+
+  if (result && callback !== undefined) {
+    callback(result)
+  }
+}
