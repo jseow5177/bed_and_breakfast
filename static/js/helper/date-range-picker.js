1d798e2d7a77463d30d40bee8650b7d7aafb2342
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-03-04 08:04:44 +0800

    Add IIFE functions to modularize code

--- static/js/helper/date-range-picker.js
+++ static/js/helper/date-range-picker.js
@@ -1,10 +1,16 @@
-// Initialise date picker for date range inputs
-export function initialiseDateRangePicker(dateRangeInputsId, format = 'yyyy-mm-dd') {
-  const dateRangeInput = document.getElementById(dateRangeInputsId)
+const DatePicker = (function () {
+  // Initialise date picker for date range inputs
+  function initialiseDateRangePicker(dateRangeInputsId, format = 'yyyy-mm-dd') {
+    const dateRangeInput = document.getElementById(dateRangeInputsId)
 
-  if (dateRangeInput === null) {
-    throw new Error('Please check if you have defined date range inputs in your HTML')
+    if (!dateRangeInput) {
+      throw new Error('Please check if you have defined date range inputs in your HTML')
+    }
+
+    new DateRangePicker(dateRangeInput, { format })
   }
 
-  new DateRangePicker(dateRangeInput, { format })
-}
+  return {
+    initialiseDateRangePicker
+  }
+})()
--- static/js/helper/form-modal.js
+++ static/js/helper/form-modal.js
@@ -1,36 +1,42 @@
-async function openFormModal({
-  title,
-  html,
-  onOpen = undefined,
-  onSubmit = undefined,
-  callback = undefined,
-  confirmButtonText = 'Ok',
-  cancelButtonText = 'Cancel',
-  showCancelButton = true,
-  showCloseButton = false,
-  allowOutsideClick = true,
-}) {
-  const result = await Swal.fire({
+const FormModal = (function () {
+  async function openFormModal({
     title,
     html,
-    didOpen: () => {
-      if (onOpen !== undefined) {
-        onOpen()
-      }
-    },
-    preConfirm: () => {
-      if (onSubmit !== undefined) {
-        onSubmit()
-      }
-    },
-    confirmButtonText,
-    cancelButtonText,
-    showCancelButton,
-    showCloseButton,
-    allowOutsideClick,
-  })
+    onOpen = undefined,
+    onSubmit = undefined,
+    callback = undefined,
+    confirmButtonText = 'Ok',
+    cancelButtonText = 'Cancel',
+    showCancelButton = true,
+    showCloseButton = false,
+    allowOutsideClick = true,
+  }) {
+    const result = await Swal.fire({
+      title,
+      html,
+      didOpen: () => {
+        if (onOpen !== undefined) {
+          onOpen()
+        }
+      },
+      preConfirm: () => {
+        if (onSubmit !== undefined) {
+          onSubmit()
+        }
+      },
+      confirmButtonText,
+      cancelButtonText,
+      showCancelButton,
+      showCloseButton,
+      allowOutsideClick,
+    })
 
-  if (result && callback !== undefined) {
-    callback(result)
+    if (result && callback !== undefined) {
+      callback(result)
+    }
   }
-}
+
+  return {
+    openFormModal
+  }
+})()
