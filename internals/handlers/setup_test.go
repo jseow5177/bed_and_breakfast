177ba2e970239728e292f05d349c608d9ee80b23
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-03-09 16:51:10 +0800

    Use TestMain function

--- internals/handlers/setup_test.go
+++ internals/handlers/setup_test.go
@@ -5,12 +5,13 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"path/filepath"
+	"testing"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/jseow5177/bed_and_breakfast/internals/config"
 	"github.com/jseow5177/bed_and_breakfast/internals/models"
 	"github.com/jseow5177/bed_and_breakfast/internals/render"
@@ -18,42 +19,36 @@ import (
 
 var app config.AppConfig
 var functions = template.FuncMap{}
+var ts *httptest.Server
 
-func getRoutes() http.Handler {
+func TestMain(m *testing.M) {
 	gob.Register(models.Reservation{})
 
-	app.InProduction = false // Change to true when in production
+	app.InProduction = false
 
-	// Initialise new session manager
 	session := scs.New()
-	session.Lifetime = 24 * time.Hour // Session lasts for 24 hours
-	session.Cookie.Persist = true // Session persists when user closes window or browser
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
 	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction // No HTTPS when in development
+	session.Cookie.Secure = app.InProduction
 
-	app.Session = session // Stores SessionManager in AppConfig so that it is accessible in other packages
+	app.Session = session
 
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
 	}
 
-	app.TemplateCache = tc // Store the template cache
+	app.TemplateCache = tc
 	app.UseCache = true
 
-	// Gives the render package access to the AppConfig instance
 	render.RegisterAppConfig(&app)
 
-	// Create a new repository for the handler package
 	repo := CreateNewRepo(&app)
-	// Gives the handlers package access to the newly created repo, which holds the AppConfig instance
 	RegisterRepo(repo)
 
-	// chi routing
-	mux := chi.NewRouter() // Create chi multiplexer
+	mux := chi.NewRouter()
 
-	// A middleware that recovers from panics, logs the panic and returns a HTTP500 status if possible
-	mux.Use(middleware.Recoverer)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", Repo.Home)
@@ -68,64 +63,44 @@ func getRoutes() http.Handler {
 	mux.Post("/search-availability", Repo.PostAvailability)
 	mux.Post("/json-test", Repo.AvailabilityJSON)
 
-	// Initiates FileServer Handler
-	// http.Dir() creates a FileSystem that points to the application static files
 	fileServer := http.FileServer(http.Dir("./static/"))
 
-	// http.StripPrefix removes "/static/" from the request URL
 	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
-	return mux
+	ts = httptest.NewServer(mux)
 }
 
-// SessionLoad is a middleware that automatically loads and saves session
-// data for the current request, and communicates the session token to and from
-// the client in a cookie.
 func SessionLoad(next http.Handler) http.Handler {
 	return app.Session.LoadAndSave(next)
 }
 
-// CreateTestTemplateCache creates a template cache as a map
 func CreateTestTemplateCache()(map[string]*template.Template, error) {
 
 	cache := make(map[string]*template.Template)
 
-	// Glob returns the names of all files matching pattern or nil if there is no matching file.
-	// Glob ignores file system errors such as I/O errors reading directories.
-	// The only possible returned error is ErrBadPattern, where pattern is malformed.
 	pages, err := filepath.Glob("../../templates/*.page.html")
 	if err != nil {
 		return cache, err
 	}
 
 	for _, page := range pages {
-		// Base returns the last element of path.
-		// Trailing path separators are removed before extracting the last element.
 		filename := filepath.Base(page)
 
-		// New allocates a new empty HTML template with the given name.
-		// template.Name() returns the name of the template.
 		newTemplate := template.New(filename)
 
-		// Funcs adds the elements of the argument map to the template's function map.
-		// A function map (FuncMap) is a map of functions that can be used in the template.
 		ts, err := newTemplate.Funcs(functions).ParseFiles("../../templates/" + filename)
 		if err != nil {
 			return cache, err
 		}
 
-		// Check if there are any layouts
 		layouts, err := filepath.Glob("../../templates/layouts/*.layout.html")
 		if len(layouts) > 0 {
-			// (*t Template) ParseGlob(pattern string) parses the template definitions in the files
-			// identified by pattern and associates the resulting templates with t.
 			ts, err = ts.ParseGlob("../../templates/layouts/*.layout.html")
 			if err != nil {
 				return cache, err
 			}
 		}
 
-		// Save template into cache
 		cache[filename] = ts
 	}
 
